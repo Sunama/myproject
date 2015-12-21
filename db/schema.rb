c94c88f06891cffa1b33e2ec844f389815208558
Sukrit Sunama <sunama.sukrit@gmail.com>
2015-12-21 15:54:02 +0700

    scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150407115224) do
+ActiveRecord::Schema.define(version: 20151221072923) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",         limit: 255
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 20150407115224) do
     t.integer  "visit_counter", limit: 4
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name",        limit: 255
+    t.string   "category",    limit: 255
+    t.float    "price",       limit: 24
+    t.text     "description", limit: 65535
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
 end
